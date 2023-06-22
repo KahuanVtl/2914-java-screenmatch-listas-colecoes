0b2e4ad91023cf8602e63d981823459c0f46fbd9
Kahuan <kahuanvitelli@gmail.com>
2023-06-22 15:02:28 -0300

    Estudando Array List

--- src/br/com/alura/screenmatch/principal/Principal.java
+++ src/br/com/alura/screenmatch/principal/Principal.java
@@ -1,3 +1,6 @@
+package br.com.alura.screenmatch.principal;
+import java.util.ArrayList;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
@@ -6,9 +9,7 @@ import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -22,18 +23,14 @@ public class Principal {
         //meuFilme.totalDeAvaliacoes = 1;
         //System.out.println(meuFilme.pegaMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("Lost", 2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
+        Filme outroFilme = new Filme("Avatar", 2023);
         outroFilme.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -50,5 +47,12 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.setAnoDeLancamento(200);
+        filmeDoPaulo.avalia(9);
+
+        
+
     }
 }
---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.principal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(5);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+    
+        for (Titulo item : lista){
+            System.out.println(item.getNome());
+                if(item instanceof Filme){
+                    Filme filme = (Filme) item;
+                    System.out.println("Classificação "+filme.getClassificacao()+" "+"Estrelas");
+            }
+        }
+        
+        System.out.println("Lista de titulos ordanados:");
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println(lista);
+
+
+    }
+}
